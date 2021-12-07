d6d58aa37566d02e987e7c3a01a617bc69607346
Arthur <sd.arthur@gmail.com>
2021-12-07 02:24:46 -0300

    Refatoração: Funcionamneto

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
 import DropDownMenu from './modules/dropdown-menu.js';
 import MenuMobile from './modules/menu-mobile.js';
-import initFuncionamento from './modules/funcionamento.js';
+import Funcionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
 
@@ -34,6 +34,9 @@ dropdown.init();
 const mobile = new MenuMobile('[data-menu="button"]', '[data-menu="lista"]');
 mobile.init();
 
-initFuncionamento();
+const funcionamento = new Funcionamento('[data-semana]', 'aberto');
+funcionamento.init();
+
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
+
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
